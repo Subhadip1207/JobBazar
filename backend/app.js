e0ae126acf4ad157b0b0497f160baf79314b4cf8
Subhadip Dutta <subhadip003.dutta@gmail.com>
2025-08-07 17:10:16 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOption = {
-    origin: 'http://localhost:5173',
+    origin: 'https://jobbazar-frontend.onrender.com',
     credentials: true                
 };
 
