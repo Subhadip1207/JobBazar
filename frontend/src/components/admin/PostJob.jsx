165f6b62d2b0f7d15d389ee6b95a1734603fc145
Subhadip Dutta <subhadip003.dutta@gmail.com>
2025-08-23 09:15:07 +0530

    Update PostJob.jsx

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -14,7 +14,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import axios from 'axios';
+import axiosInstance from "../utils/axiosInstance.js";
 import { JOB_API_ENDPOINT } from '../../utils/constant';
 import { toast } from 'sonner';
 import { useNavigate } from 'react-router-dom';
@@ -51,7 +51,7 @@ const PostJob = () => {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await axios.post(`${JOB_API_ENDPOINT}/post`, input, {
+      const res = await axiosInstance.post(`${JOB_API_ENDPOINT}/post`, input, {
         headers: { 'Content-Type': 'application/json' },
         withCredentials: true,
       });
