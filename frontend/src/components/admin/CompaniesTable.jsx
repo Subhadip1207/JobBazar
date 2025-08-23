94e39214d447810a84aad9035a05a69d0087982e
Subhadip Dutta <subhadip003.dutta@gmail.com>
2025-08-23 09:36:08 +0530

    Update CompaniesTable.jsx

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -11,7 +11,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { COMPANY_API_ENDPOINT } from '../../utils/constant';
 import { toast } from 'sonner';
-import axiosInstance from "../utils/axiosInstance.js";
+import axiosInstance from "../../utils/axiosInstance.js";
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { setCompanies } from '../../redux/companySlice';
